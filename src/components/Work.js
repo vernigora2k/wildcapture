917736ae71c5a0b99d1390a8123f947af079baa1
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-01-15 14:14:08 +0200

    Added work button more-works functional

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,14 +4,16 @@ import './Work.scss'
 export const Work = () => {
     const [isMoreWorks, setIsMoreWorks] = useState(false);
 
-    const workVideos = [
-        {name1: 'work-video-item.png'},
-        {name2: 'work-video-item.png'},
-        {name3: 'work-video-item.png'},
-        {name4: 'work-video-item.png'},
-        {name5: 'work-video-item.png'},
-        {name6: 'work-video-item.png'},
-    ]
+    const workVideos = {
+        name1: 'work-video-item.png',
+        name2: 'work-video-item.png',
+        name3: 'work-video-item.png',
+        name4: 'work-video-item.png',
+        name5: 'work-video-item.png',
+        name6: 'work-video-item.png',
+        name7: 'work-video-item.png',
+        name8: 'work-video-item.png',
+    }
 
     return (
         <section className="work">
@@ -19,17 +21,26 @@ export const Work = () => {
                 <div className="work__layer">
                 <div className="work__title">Real Digital People</div>
                     <div className="work__videos">
-                        {Object.entries(workVideos).map(item => {return (
+                        {!isMoreWorks ? 
+                            Object.entries(workVideos).slice(1,4).map(([name, value]) => {return (
                             <div className="work__videos-item">
                                 <span className="videos-item-caption">
-                                    {console.log(item[1])}
-                                    {item[0]}
+                                    {name}
                                 </span>
-                                <div className="videos-item-content">
-
+                                <div className="videos-item-content" style={{ backgroundImage: `url(img/${value})` }}>
                                 </div>
                             </div>
-                        )})}
+                            )}) 
+                        :   Object.entries(workVideos).map(([name, value]) => {return (
+                            <div className="work__videos-item">
+                                <span className="videos-item-caption">
+                                    {name}
+                                </span>
+                                <div className="videos-item-content" style={{ backgroundImage: `url(img/${value})` }}>
+                                </div>
+                            </div>
+                            )}) 
+                        }
                     </div>
                     
                 </div>
