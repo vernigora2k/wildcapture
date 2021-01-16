8135d553f734898f8595e7ea7849455766400073
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-01-17 01:08:21 +0200

    Finished with Partners component

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -2,16 +2,32 @@ import React, { Fragment, useState } from 'react';
 import './Partners.scss'
 
 export const Partners = () => {
+    const workVideos = {
+        logo1: 'partners-logo-WMR.png',
+        logo2: 'partners-logo-TetaVi.png',
+        logo3: 'partners-logo-sidefx.svg',
+        logo4: 'partners-logo-quixel.png',
+        logo5: 'partners-logo-evercoast.png',
+        logo6: 'partners-logo-epicgames.png',
+        logo7: 'partners-logo-DNE.png',
+        logo8: 'partners-logo-captury.png',
+        logo9: 'partners-logo-4d.svg',
+    }
     
     return (
         <section className="partners">
             <div className="partners__shell">
                 <div className="partners__layer">
                     <div className="partners__title">Partners</div>
-                   
-                    
-                    <div className="partners__logo">
-                        <img src={"../img/work-main-image.svg"} />
+                    <div className="partners__main">
+                        {Object.entries(workVideos).map(([name, value]) => {return (
+                            <div className="partners__item-shell">
+                                <div className="partners__item" style={{ backgroundImage: `url(img/${value})` }}></div>
+                            </div>    
+                        )})}
+                    </div>
+                    <div className="partners__image">
+                        <img src={"../img/partners-main-image.svg"} />
                     </div>
                     
                 </div>
