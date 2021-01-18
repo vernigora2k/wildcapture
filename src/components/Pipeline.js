b010a65af3a480ebaad1dddd2e2447ceee2e5a5b
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-01-18 13:29:17 +0200

    Added pipeline component adoptive

--- src/components/Pipeline.js
+++ src/components/Pipeline.js
@@ -10,27 +10,27 @@ export const Pipeline = () => {
                 <div className="pipeline__layer">
                     <div className="pipeline__title">Pipeline</div> 
                     <div className="pipeline__main">
-                        <div className="pipeline__item">
+                        <div className="pipeline__item pipeline__item1">
                             <span className="pipeline__item-caption">Capture</span>
                             <span className="pipeline__item-description">Shoot Content Survey Performance or Location Data</span>
                             <span className="pipeline__item-number">1</span>
                         </div>
-                        <div className="pipeline__item">
+                        <div className="pipeline__item pipeline__item2">
                             <span className="pipeline__item-caption">Processing</span>
                             <span className="pipeline__item-description">Conforming Content at Highest Quality Standards</span>
                             <span className="pipeline__item-number">2</span>
                         </div>
-                        <div className="pipeline__item">
+                        <div className="pipeline__item pipeline__item3">
                             <span className="pipeline__item-caption">AI</span>
                             <span className="pipeline__item-description">Optimize Content & Provide Skeletal and Segmentation Data, Mesh Uniform Solver</span>
                             <span className="pipeline__item-number">3</span>
                         </div>
-                        <div className="pipeline__item">
+                        <div className="pipeline__item pipeline__item4">
                             <span className="pipeline__item-caption">Post production & engineering</span>
                             <span className="pipeline__item-description">Asset Prep & Cleanup, Scene Integration, Interactivity & sound. </span>
                             <span className="pipeline__item-number">4</span>
                         </div>
-                        <div className="pipeline__item">
+                        <div className="pipeline__item pipeline__item5">
                             <span className="pipeline__item-caption">Delivery</span>
                             <span className="pipeline__item-description">Deliver Best Quality with Extreme Versatility</span>
                             <span className="pipeline__item-number">5</span>
