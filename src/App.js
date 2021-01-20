ff3b39e1880fdbfe0aa757bd2abac28390bf77c4
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-01-20 14:11:32 +0200

    Change favicon

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
