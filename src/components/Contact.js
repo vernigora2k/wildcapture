cb0ee22213ec61f31849ba7cb599a52a7cfe76a8
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-01-21 11:38:07 +0200

    Added contact inputYourCompany checking

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.scss';
 import { NavLink } from 'react-router-dom';
 import emailjs from 'emailjs-com';
 
 export const Contact = () => {
+    // const [inputYourCompany, setInputYourCompany] = useState('')
+    const [inputYourCompanyFocus, setInputYourCompanyFocus] = useState(false)
+    const [inputYourCompanyDirty, setInputYourCompanyDirty] = useState(false)
+    const [errorInputYourCompany, setErrorInputYourCompany] = useState(false)
+
+    const focusInputHandle = (e) => {
+        switch (e.target.name) {
+            case 'company':
+                setInputYourCompanyFocus(true)
+                break
+        }
+    }
+
+    const blurInputHandle = (e) => {
+        switch (e.target.name) {
+            case 'company':
+                setInputYourCompanyDirty(true)
+                break
+        }
+    }
+
     const sendMessage = (e) => {
         e.preventDefault()
 
@@ -42,11 +63,21 @@ export const Contact = () => {
                             </div>
                         </div>
                         <form className="contact__form" onSubmit={sendMessage}>
-                            <input className="contact__input" type="text" placeholder="Your name" name="name" />
-                            <input className="contact__input" type="text" placeholder="Your email" name="email" />
-                            <label className="label-input label-input-your-company hidden" for="input-your-company">Your company</label>
-                            <input id="input-your-company" className="contact__input" type="text" placeholder="Your company" name="company" />
-                            <textarea className="contact__textarea" placeholder="How can we help.." name="message" />
+                            <input className="contact__input" required type="text" placeholder="Your name" name="name" />
+                            <input className="contact__input" required type="text" placeholder="Your email" name="email" />
+                            {inputYourCompanyFocus  && 
+                                <label className="label-input label-input-your-company" htmlFor="input-your-company">Your company</label>
+                            }
+                            <input 
+                              id="input-your-company" 
+                              onFocus={focusInputHandle} 
+                              onBlur={blurInputHandle} 
+                              className={inputYourCompanyDirty ? 'contact__input contact__input-visited' : 'contact__input'} 
+                              type="text" 
+                              placeholder="Your company" 
+                              name="company" 
+                            />
+                            <textarea className="contact__textarea" required placeholder="How can we help.." name="message" />
                             <input type="submit" className="button contact__submit" id="contact__submit" value="Send" />
                         </form>
                     </div>
