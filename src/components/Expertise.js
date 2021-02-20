a545fea818e3db9e2ee240596879babded44a4c4
Wilfred Driscoll <wd@wildcapture.co>
2021-02-20 13:45:33 -0800

    fixed pages for upload

--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -10,28 +10,31 @@ export const Expertise = () => {
                     <div className="expertise__main">
                         <div className="expertise__item">
                             <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-right.svg"} />
-                            <span>Realistic CG Characters for Virtual</span>
-                            <span>Production, Broadcast,</span>
-                            <span>Film,</span>
-                            <span>Experiential & Mixed Reality</span>
+                            <span>Volumetric Video Capture</span>
+                            <span>Markerless Motion Capture</span>
+                            <span>Pose Estimation & Cloth Segmentation</span>
+                            <span>Mesh and UV Sequence Uniformity</span>
                         </div>
                         <div className="expertise__item">
-                            <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-bottom.svg"} />
-                            <span>A to Z Realtime Production Pipeline Solutions</span>
-                            <span>Color, Light and Photography Mastery</span>
+                            <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-bottom.png"} />
+                            <span>Virtual Production Consultation Services</span>
+                            <span>Design, Software Deployment & Installation</span>
+                            <span>Expert Onsite Supervision</span>
+                            <span>Data Mangement and IT Services</span>
                         </div>
                         <div className="expertise__item">
-                            <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-top.svg"} />
-                            <span>Character AI & Crowd Logic for Production,</span>
-                            <span>Game Engine Plugins,</span>
+                            <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-top.png"} />
+                            <span>Character AI & Crowd Logic for Production</span>
                             <span>Virtual Events and Simulated Live Audience</span>
+                            <span>Universal Plugin Development</span>
+                            <span>Realtime & USD Render Pipelines</span>
                         </div>
                         <div className="expertise__item">
-                            <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-left.svg"} />
-                            <span>Delivery to All Formats and Mediums,</span>
-                            <span>Deploy to All Devices, </span>
-                            <span>WebGL Expertise, </span>
-                            <span>Game Authoring</span>
+                            <img className="expertise__puzzle-image" src={"../img/expertise-puzzle-left.png"} />
+                            <span>Predictive & Physically Accurate CG Cloth</span>
+                            <span>Logo Branding and Texture Modification</span>
+                            <span>Hair, Prop and Jewelry Integration</span>
+                            <span>Compatible with WebGL</span>
                         </div>
                     </div>
                     <div className="expertise__left-image">
--- src/components/Pipeline.js
+++ src/components/Pipeline.js
@@ -10,29 +10,29 @@ export const Pipeline = () => {
                     <div className="pipeline__title">Pipeline</div> 
                     <div className="pipeline__main">
                         <div className="pipeline__item pipeline__item1">
-                            <span className="pipeline__item-caption">Capture</span>
-                            <span className="pipeline__item-description">Shoot Content Survey Performance or Location Data</span>
-                            <span className="pipeline__item-number">1</span>
+                        <span className="pipeline__item-caption">Strategy</span>
+                            <span className="pipeline__item-description">Taking any concept to a feasible execution plan for cutting edge production</span>
+                            <span className="pipeline__item-number"></span>
                         </div>
                         <div className="pipeline__item pipeline__item2">
-                            <span className="pipeline__item-caption">Processing</span>
-                            <span className="pipeline__item-description">Conforming Content at Highest Quality Standards</span>
-                            <span className="pipeline__item-number">2</span>
+                            <span className="pipeline__item-caption">Capture</span>
+                            <span className="pipeline__item-description">Capturing the Human Essence with the highest quality standards</span>
+                            <span className="pipeline__item-number"></span>
                         </div>
                         <div className="pipeline__item pipeline__item3">
-                            <span className="pipeline__item-caption">AI</span>
-                            <span className="pipeline__item-description">Optimize Content & Provide Skeletal and Segmentation Data, Mesh Uniform Solver</span>
-                            <span className="pipeline__item-number">3</span>
+                            <span className="pipeline__item-caption">Process</span>
+                            <span className="pipeline__item-description">Using data to bring vibrant life to otherwise lifeless CG</span>
+                            <span className="pipeline__item-number"></span>
                         </div>
                         <div className="pipeline__item pipeline__item4">
-                            <span className="pipeline__item-caption">Post production & engineering</span>
-                            <span className="pipeline__item-description">Asset Prep & Cleanup, Scene Integration, Interactivity & sound. </span>
-                            <span className="pipeline__item-number">4</span>
+                            <span className="pipeline__item-caption">Post</span>
+                            <span className="pipeline__item-description">Preparing Assets for Prime Time with vast functionality and incredible efficiency</span>
+                            <span className="pipeline__item-number"></span>
                         </div>
                         <div className="pipeline__item pipeline__item5">
                             <span className="pipeline__item-caption">Delivery</span>
-                            <span className="pipeline__item-description">Deliver Best Quality with Extreme Versatility</span>
-                            <span className="pipeline__item-number">5</span>
+                            <span className="pipeline__item-description">Delivering to Film, TV, Games, Experiences, Installations, and a device on you.</span>
+                            <span className="pipeline__item-number"></span>
                         </div>
                     </div>
                     <div className="pipeline__bottom-image">
