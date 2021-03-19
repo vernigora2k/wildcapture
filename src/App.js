8cf71d95eb548b690b30b40dd86de1ef7b3a149e
Wilfred Driscoll <wd@wildcapture.co>
2021-03-18 22:52:20 -0700

    added SamB volumetric page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { WhatWeDo } from './pages/WhatWeDo';
 import { Volumetric } from './pages/Volumetric';
+import { SamB } from './pages/SamB';
 
 function App() {
   useEffect(() => {
@@ -18,6 +19,7 @@ function App() {
           <Route path={'/'} exact component={Home}></Route>
           <Route path={'/whatwedo'} component={WhatWeDo}></Route>
           <Route path={'/volumetric'} component={Volumetric}></Route>
+          <Route path={'/vol-samb-url'} component={SamB}></Route>
         </Switch>
       </div>
     </BrowserRouter>
